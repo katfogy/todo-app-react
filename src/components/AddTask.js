1c1cb127c54ed3d0232c9d17a6736179d88f5477
katfogy <katfogy@gmail.com>
2023-06-06 02:18:26 +0100

    custome error message added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,19 +3,25 @@ import React, { useState } from 'react';
 const AddTask = (prop) => {
   const { AddNewTask } = prop;
   const [tasknew, setNewTask] = useState('');
+  const [message, setMessage] = useState('');
   const onChange = (e) => {
     setNewTask(e.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    AddNewTask(tasknew);
-    setNewTask('');
+    if (!tasknew) {
+      setMessage('Todo Title Cannot be empty');
+    } else {
+      AddNewTask(tasknew);
+      setNewTask('');
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Please Add Todo" value={tasknew} onChange={onChange} />
       <button type="submit">Add Todo</button>
+      <span>{message}</span>
     </form>
   );
 };
